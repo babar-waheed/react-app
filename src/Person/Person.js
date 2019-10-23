2e04298673d5d3fff68077df710a4813cb70d5b5
Babar Waheed <babar.waheed@gmail.com>
2019-10-23 13:25:01 +1100

    Understanding the Children Property (props.children)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I'm {props.name} and I'm {props.age} years old.</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old.</p>
+            {/*children refers to any element inside the opening and closing tags e.g below*/}
+            {/*<Person name="John" age="34"> My Hobbies: Coding </Person>*/}
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
