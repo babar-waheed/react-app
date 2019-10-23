3c5ae1909798659e0c859dffc776dc3a2b6946e0
Babar Waheed <babar.waheed@gmail.com>
2019-10-23 12:48:15 +1100

    JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      //JSX Restrictions
+      //class can't be used. React is converting the html behind the scene.
+      //JSX must have one root element
       <div className="App">
         <h1>I'm a React App</h1>
       </div>
     );
+
+    //return(<div className="App"><h1>Hello!</h1></div>);
+    //Above code is equal to below
+   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello!'));
+
   }
 }
+console.log(React);
 
 export default App;
