dc115382e5d4f9a21e3559d9cf3a24b47523470d
Babar Waheed <babar.waheed@gmail.com>
2019-10-23 13:47:40 +1100

    Understanding & Using State

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      //JSX Restrictions
-      //class can't be used. React is converting the html behind the scene.
-      //JSX must have one root element
-      <div className="App">
-        <h1>I'm a React App</h1>
-        <Person name="Paul" age="23"/>
-        <Person name="John" age="34"> My Hobbies: Coding </Person>
-      </div>
+    //Understanding and Using State
+    //Whilst props allow you to pass data down the component tree (and hence trigger an UI update), state is used to change the component, well, state from within. Changes to state also trigger an UI update.
+    state = {
+        counter: 1,
+        person: [
+            { name: 'Paul', age: '28'},
+            { name: 'John', age: '29'}
+        ]
+    };
+    render() {
+        return (
+        //JSX Restrictions
+        //class can't be used. React is converting the html behind the scene.
+        //JSX must have one root element
+        <div className="App">
+            <h1>I'm a React App {this.state.counter}</h1>
+            <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+            <Person name={this.state.person[1].name} age={this.state.person[1].age}> My Hobbies: Coding </Person>
+        </div>
     );
 
     //return(<div className="App"><h1>Hello!</h1></div>);
@@ -21,6 +30,5 @@ class App extends Component {
 
   }
 }
-console.log(React);
 
 export default App;
