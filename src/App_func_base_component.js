d5ee8235b49e33dccb17b6772efc4ed166419a23
Babar Waheed <babar.waheed@gmail.com>
2019-10-23 15:31:46 +1100

    Function Components & useState() Hook for State Manipulation

---
+++ src/App_func_base_component.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const App = (props) => {
+
+    const [personState, setPersonState] = useState({
+        person: [
+            { name: 'Paul', age: '28'},
+            { name: 'John', age: '29'}
+        ]
+    });
+
+    const [counterState, setCounterState] = useState({
+        counter: 1
+    });
+
+    const switchNameHandler = () => {
+        setPersonState({
+            person: [
+                { name: 'Andrew', age: '25'},
+                { name: 'Ash', age: '35'}
+            ]
+        })
+        setCounterState({counter: 2});
+    };
+
+    return (
+        <div className="App">
+            <h1>I'm a React App {counterState.counter}</h1>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person
+                name={personState.person[0].name}
+                age={personState.person[0].age}/>
+            <Person
+                name={personState.person[1].name}
+                age={personState.person[1].age}>
+                My Hobbies: Coding
+            </Person>
+        </div>
+    );
+};
+
+export default App;
+
+
+
+
