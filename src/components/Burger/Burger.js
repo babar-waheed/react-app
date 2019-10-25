eb34a5d2720c4afbc133372e13df650437504182
Babar Waheed <babar.waheed@gmail.com>
2019-10-26 10:22:47 +1100

    Addition of controls to add ingredients to the burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) => {
@@ -13,7 +13,15 @@ const burger = (props) => {
                         key={igKey + i}
                     />
                 });
-        });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+    }, []);
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
