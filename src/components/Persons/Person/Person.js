6d877e64ba4dab3749d98183bb4820a8c8277bfa
Babar Waheed <babar.waheed@gmail.com>
2019-10-25 11:38:24 +1100

    Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Style from './Person.css';
 import Aux from '../../../hoc/Aux';
 
@@ -7,15 +7,13 @@ class Person extends Component {
         console.log('[Person.js render()]');
 
         return(
-            <Aux>
-                <div className={Style.Person}>
-                    <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
-                    {/*children refers to any element inside the opening and closing tags e.g below*/}
-                    {/*<Person name="John" age="34"> My Hobbies: Coding </Person>*/}
-                    <p>{this.props.children}</p>
-                    <input onChange={this.props.change} type="text" name="name" value={this.props.name} />
-                </div>
-            </Aux>
+            <Fragment>
+                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old.</p>
+                {/*children refers to any element inside the opening and closing tags e.g below*/}
+                {/*<Person name="John" age="34"> My Hobbies: Coding </Person>*/}
+                <p>{this.props.children}</p>
+                <input onChange={this.props.change} type="text" name="name" value={this.props.name} />
+            </Fragment>
         )
     }
 }
