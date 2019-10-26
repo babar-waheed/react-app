c016808dd8cc67777003676c02613ffe0b178d19
Babar Waheed <babar.waheed@gmail.com>
2019-10-26 16:48:43 +1100

    Ajax project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I'm a React App</h1>
+        <Blog />
       </div>
     );
   }
