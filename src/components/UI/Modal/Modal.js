c362fc03447f6858c8cbd23610d4d42452e38dab
Babar Waheed <babar.waheed@gmail.com>
2019-10-26 13:43:49 +1100

    Performance improvement using Lifecycle hook shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import Aux from '../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
+class Modal extends Component{
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop
-            show={props.show}
-            clicked={props.modalClosed}
-        />
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : "translateY(-100vh)",
-                opacity: props.show ?  '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("[Modal] componentDidUpdate");
+    }
 
-);
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render(){
+        return(
+            <Aux>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed}
+                />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : "translateY(-100vh)",
+                        opacity: this.props.show ?  '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+
+export default Modal;
