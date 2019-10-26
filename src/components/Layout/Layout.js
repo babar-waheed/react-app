2f716e41ac09d90a08a0c80bf865ce5675940c01
Babar Waheed <babar.waheed@gmail.com>
2019-10-26 13:22:48 +1100

    Mobile Responsive Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import Aux from '../hoc/Aux';
+import React, { Component } from 'react';
+
+import Aux from '../hoc/Aux/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    };
 
-    </Aux>
-);
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    };
 
-export default layout;
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    };
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
+
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
-import Nav from '../../Navigation/NavigationItems/NavigationItems';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = (props) => {
-    return (
-        <header className={classes.Toolbar}>
-            <div>Menu</div>
+const toolbar = ( props ) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
             <Logo />
-            <Nav />
-        </header>
-    );
-};
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+    </header>
+);
 
-export default toolbar
+export default toolbar;
