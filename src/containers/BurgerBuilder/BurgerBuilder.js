bd815b32086f3ca348dc3149d52a9ddd7391e3e7
Babar Waheed <babar.waheed@gmail.com>
2019-10-26 11:46:44 +1100

    Hide and Show order summary modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,8 @@ class BurgerBuilder extends Component{
             meat: 0
         },
         totalPrice: 5,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     };
 
     updatePurchaseState (ingredients) {
@@ -78,6 +79,10 @@ class BurgerBuilder extends Component{
         this.updatePurchaseState(updatedIngredients);
     };
 
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    };
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients,
@@ -88,7 +93,7 @@ class BurgerBuilder extends Component{
         }
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary
                         ingredients={this.state.ingredients}
                     />
@@ -100,6 +105,7 @@ class BurgerBuilder extends Component{
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
         )
